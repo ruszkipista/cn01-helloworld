804f4bdd775b979c61d9dc09ef60e05d53ea327b
ruszkipista <oi5672@gmail.com>
2021-06-21 16:10:06 +0200

    add logging of endpoint triggers

--- app.py
+++ app.py
@@ -1,30 +1,38 @@
 from flask import Flask
 from flask import json
+import logging
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
+    app.logger.info('Main request successfull')
     return "Hello World!"
+
 
 @app.route('/status')
 def status():
+    app.logger.info('Status request successfull')
     response = app.response_class(
             response=json.dumps({"result":"OK - healthy"}),
             status=200,
             mimetype='application/json'
     )
-
     return response
+
 
 @app.route('/metrics')
 def metrics():
+    app.logger.info('Metrics request successfull')
     response = app.response_class(
             response=json.dumps({"status":"success","code":0,"data":{"UserCount":140,"UserCountActive":23}}),
             status=200,
             mimetype='application/json'
     )
-
     return response
 
+
 if __name__ == "__main__":
+    ## stream logs to app.log file
+    logging.basicConfig(filename='app.log',level=logging.DEBUG)
+    
     app.run(host='127.0.0.1', port='5500')
